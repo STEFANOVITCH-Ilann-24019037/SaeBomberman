8ae61517e71b26883f36588782b4c6e3af280248
STEFANOVITCH-Ilann-24019037 <ilann.stefanovitch@etu.univ-amu.fr>
2025-06-01 11:06:45 +0200

    sup de SeaBomberman

--- SaeBomberman/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module fr.ilannstefanovitch.saebomberman {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports fr.ilannstefanovitch.saebomberman;
-    opens fr.ilannstefanovitch.saebomberman to javafx.fxml;
-}
